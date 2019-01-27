0e729f0b814555175a5f8884303630decde63ace
MHessenius <mhessenius@web.de>
2019-01-27 19:33:22 +0100

    Update MainFile.R
    Sketch of a code structure

--- MainFile.R
+++ MainFile.R
@@ -6,8 +6,40 @@
 
 ownwd <- "C:/SPL_local" # your personal folder
 setwd(ownwd) # set working directory
+source("LoadPackages.R") # load packages
 
+#####################################
+#     1) PreProcessing
+#####################################
+
+# Get the Dataset & merging
 source("FirstSteps.R")
+
+# Calculate the HitRate
 source("HitRate.R")
-source("LoadPackages.R")
+
+# Create InteractionTerms
+source(....)
+
+########################################
+#     2) CLEANING
+########################################
+
+# missValues & Outliers
+
+# to be done!
+
+###########################################
+#     3) ANALYSIS
+############################################
+
+# logistic regression
+# to be done....
+
+##########################################
+#     4) PostProcessing
+############################################
+
+# plots, tables, nice output etc
+
 
