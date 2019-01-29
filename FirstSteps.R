54e386d851d1c74aedb739a6f47b21904caf78b6
MHessenius <mhessenius@web.de>
2019-01-30 00:30:31 +0100

    structuring

--- FirstSteps.R
+++ FirstSteps.R
@@ -1,80 +1,78 @@
-rm(list = ls()) #remove all current objects to clear workspace
+# LOAD ANNUAL DATASETS
 
-# NEED FOR CHANGE - SET YOUR WORKING DIRECTORY!
-ownwd <- "C:/Users/Malte/Desktop/Uni/SPL/nypd-stopfrisk" # not necessary if working with GitHub
+  # (previously converted into .rds format to reduce size)
+  
+  nypd2013 <- readRDS("./Data-rds/sqf2013.rds")
+  nypd2014 <- readRDS("./Data-rds/sqf2014.rds")
+  nypd2015 <- readRDS("./Data-rds/sqf2015.rds")
+  nypd2016 <- readRDS("./Data-rds/sqf2016.rds")
 
+# COMBINE AND SUBSET DATA FRAME
 
-# Prework
-setwd(ownwd) # set working directory
-
-
-# load annual datesets (previously converted into .rds format to reduce size)
-
-nypd2013 <- readRDS("./Data-rds/sqf2013.rds")
-nypd2014 <- readRDS("./Data-rds/sqf2014.rds")
-nypd2015 <- readRDS("./Data-rds/sqf2015.rds")
-nypd2016 <- readRDS("./Data-rds/sqf2016.rds")
-
-# select relevant variables as stated in the paper:
-# sex, race, build, trhsloc (housing or transit), inout (inside or outside), 
-# year, datestop, timestop
-# all vars beginning with  "cs_" = reason for stop
-# radio; offunif; ht_feet ; ht_inch ; weight; age; perobs
-# columns indicating whether a weapon was found: pistol, riflshot, asltweap, knifcuti, machgun, othrweap
-
-weaponVars <- c("pistol", "riflshot", "asltweap", "knifcuti", "machgun", "othrweap")
-
-covariates <- c("year","datestop","timestop","pct","sex","race","ht_feet", "ht_inch", "weight", "age", 
-                "trhsloc","inout","offunif","perobs",grep("cs_",names(nypd2014), value=TRUE), "radio",
-                "crimsusp", #crimsusp is needed to construct dependent variable
-                weaponVars) # weaponVars necessary to construct dependent variable (= weapon found or not)
-                
-
-# to be done: (2) local hit rate as described below
-
-# rename columns if necessary
-names(nypd2016)[1] = "year"   # column had a wrong name (= i...year)
-
-# combine all rows into one file 
-df <- do.call(rbind,mget(ls(pattern = "nypd*")))   # combine all rows
-rm(list=ls(pattern = "nypd*"))  # remove old files
-
-# only keep relevant covariates
-df <- subset(df,select=covariates)
-
-# some post-processing (ht_feet & ht_inch to centimeters)
-df$ht_feet <- df$ht_feet*30.48 # convert feet to cm
-df$ht_inch <- df$ht_inch*2.54 #convert inch to cm
-df$height <- df$ht_feet+df$ht_inch
-df[,c("ht_feet","ht_inch")] <- NULL
-range(df$height,na.rm = TRUE) #between 91.44 and 241.3 cm -> mistakes!?
-
-#  transformation of variables if necessary
-
-str(df) # have a look on classes of columns
-df$age <- as.numeric(df$age) # age has to be numeric or integer instead of character
-
-# NORMALIZATION 
-# Paper: "the latter four are all normalized to have mean 0 and variance 1" 
-# -> height, weight, age, perobs
-normalizeVars <- c("height","weight","age","perobs")
-df[,normalizeVars] <- scale(df[,normalizeVars]) # scale does the job
-lapply(df[,normalizeVars], mean, na.rm=TRUE) #Verification: all means are (almost) zero
-
-# -> we are only interested in "CPW"as suspected crime
-
-StrToMatch <- c("CPW","C.P.W","WEAPON","GUN","FIREARM") # assign patterns that indicate "CPW", add more!
-df$CPW <- ifelse(grepl(paste(StrToMatch,collapse="|"),df$crimsusp)==TRUE,1,0)
-length(which(df$CPW==1)) #33527, correct!
-
-# only keep df$CPW==1
-df <- df[df$CPW==1,]
-
-# dependent variable: weapon found or not (pistol, rifle, asltweap, knife, mchngun, other weapon)
-df$weaponfound <- ifelse(df$pistol=="Y" | df$riflshot=="Y" | df$asltweap=="Y" | df$knifcuti=="Y" | df$machgun=="Y" | df$othrweap=="Y", 1,0)
+  # select relevant variables as stated in the paper:
+  # sex, race, build, trhsloc (housing or transit), inout (inside or outside), 
+  # year, datestop, timestop
+  # all vars beginning with  "cs_" = reason for stop
+  # radio; offunif; ht_feet ; ht_inch ; weight; age; perobs
+  # columns indicating whether a weapon was found: pistol, riflshot, asltweap, knifcuti, machgun, othrweap
+  
+  weaponVars <- c("pistol", "riflshot", "asltweap", "knifcuti", "machgun", "othrweap")
+  
+  covariates <- c("year","datestop","timestop","pct","sex","race","ht_feet", "ht_inch", "weight", "age", 
+                  "trhsloc","inout","offunif","perobs",grep("cs_",names(nypd2014), value=TRUE), "radio",
+                  "crimsusp", #crimsusp is needed to construct dependent variable
+                  weaponVars) # weaponVars necessary to construct dependent variable (= weapon found or not)
+  
+  # rename columns if necessary
+  names(nypd2016)[1] = "year"   # column had a wrong name (= i...year)
+  
+  # combine all rows into one file 
+  df <- do.call(rbind,mget(ls(pattern = "nypd*")))   # combine all rows
+  rm(list=ls(pattern = "nypd*"))  # remove old files
+  
+  # only keep relevant covariates
+  df <- subset(df,select=covariates)
+  
+  # on top, we are only interested in "CPW"as suspected crime
+  
+  StrToMatch <- c("CPW","C.P.W","WEAPON","GUN","FIREARM") # assign patterns that indicate "CPW", add more!
+  df$CPW <- ifelse(grepl(paste(StrToMatch,collapse="|"),df$crimsusp)==TRUE,1,0)
+  length(which(df$CPW==1)) #33527, correct!
+  
+  # only keep CPW as suspected crime
+  df <- df[df$CPW==1,]
+  
+  # DEPENDENT VARIABLE: weapon found or not 
+  # respective columns: pistol, rifle, asltweap, knife, mchngun, other weapon
+  
+  df$weaponfound <- ifelse(df$pistol=="Y" | df$riflshot=="Y" | df$asltweap=="Y" | df$knifcuti=="Y" | df$machgun=="Y" | df$othrweap=="Y", 1,0)
   df[,c(weaponVars)] <- NULL  # no use for weaponVars anymore
 
-# ... to be continued
+# SOME POST-PROCESSING
 
-saveRDS(df,file="nypd2013_2016.rds")
+  # e.g. transformation and appropriate classes
+  
+  # some post-processing (ht_feet & ht_inch to centimeters)
+  df$ht_feet <- df$ht_feet*30.48 # convert feet to cm
+  df$ht_inch <- df$ht_inch*2.54 #convert inch to cm
+  df$height <- df$ht_feet+df$ht_inch
+  df[,c("ht_feet","ht_inch")] <- NULL
+  range(df$height,na.rm = TRUE) #between 91.44 and 241.3 cm -> mistakes!?
+  
+  #  transformation of variables if necessary
+  
+  str(df) # have a look on classes of columns
+  df$age <- as.numeric(df$age) # age has to be numeric or integer instead of character
+  
+  # NORMALIZATION 
+  # Paper: "the latter four are all normalized to have mean 0 and variance 1" 
+  # -> height, weight, age, perobs
+  normalizeVars <- c("height","weight","age","perobs")
+  df[,normalizeVars] <- scale(df[,normalizeVars]) # scale does the job
+  lapply(df[,normalizeVars], mean, na.rm=TRUE) #Verification: all means are (almost) zero
+
+
+# SAVE FILE FOR FURTHER PROCESSING (cleaning etc.)
+
+  saveRDS(df,file="nypd2013_2016.rds")
 
--- MainFile.R
+++ MainFile.R
@@ -1,3 +1,4 @@
+rm(list = ls()) #remove all current objects to clear workspace
 # **********************************
 #       MAIN FILE 
 # **********************************
@@ -6,7 +7,7 @@
 
 ownwd <- "C:/SPL_local" # your personal folder
 setwd(ownwd) # set working directory
-source("LoadPackages.R") # load packages
+source("LoadPackages.R") # load all required packages
 
 #####################################
 #     1) PreProcessing
@@ -15,98 +16,27 @@ source("LoadPackages.R") # load packages
 # Get the Dataset & merging
 source("FirstSteps.R")
 
-# Calculate the HitRate
+# Covariates: HitRate
 source("HitRate.R")
 
-# Create InteractionTerms
-source(....)
+# Covariates: Pairwise Interaction Terms
+source(...)
+
+# Cleaning
+source("Cleaning.R")
 
 ########################################
-#     2) CLEANING
+#     2) ANALYSIS
 ########################################
 
-# Check and correct for missing values
-
-colSums(is.na.data.frame(df))
-sort(table(df$age)) # -0.317192507 is the most frequent value for standardized age.
-df$age [is.na(df$age)] <- -0.317192507
-
-# Define a function for standardization
-standardize <- function(x){
-  
-  mu <- mean(x)
-  std <- sd(x)
-  result <- (x - mu)/std
-  
-  return(result)   }
-
-# Outlier treatment
-
-boxplot(df$weight)
-boxplot(df$height)
-boxplot(df$perobs)
-
-#  Replace the assumed outliers in standardized age, weight, height and perobs with a threshold value.
-
-df$age [df$age > 3] <- mean(df$age) + 3*sd(df$age)
-df$weight [df$weight > 3] <- mean(df$weight) + 3*sd(df$weight)
-df$height [df$height > 3] <- mean(df$height) + 3*sd(df$height)
-df$perobs [df$perobs > 3] <- mean(df$perobs) + 3*sd(df$perobs)
-
-
- # Month variable
-install.packages("lubridate")
-library(lubridate)
-df$month <- month(dmy(df$datestop))
-
-# Weekday variable
-
-install.packages("anytime")
-as.character(df$datestop)
-date <- anytime:: anydate (df$datestop)
-df$weekday <- wday(date, label=TRUE)
-
-# Time of the day variable
-
-time <- anytime:: anydate (df$timestop)
-
-
-
-# Standardizing the entries for reasons for stops 
-
-df$cs_objcs [df$cs_objcs == ""] <- "N"
-df$cs_objcs [df$cs_objcs == "1"] <- "Y"
-df$cs_descr [df$cs_descr == ""] <- "N"
-df$cs_descr [df$cs_descr == "1"] <- "Y"
-df$cs_casng [df$cs_casng == ""] <- "N"
-df$cs_casng [df$cs_casng == "1"] <- "Y"
-df$cs_lkout [df$cs_lkout == ""] <- "N"
-df$cs_lkout [df$cs_lkout == "1"] <- "Y"
-df$cs_cloth [df$cs_cloth == ""] <- "N"
-df$cs_cloth [df$cs_cloth == "1"] <- "Y"
-df$cs_drgtr [df$cs_drgtr == ""] <- "N"
-df$cs_drgtr [df$cs_drgtr == "1"] <- "Y"
-df$cs_furtv [df$cs_furtv == ""] <- "N"
-df$cs_furtv [df$cs_furtv == "1"] <- "Y"
-df$cs_vcrim [df$cs_vcrim == ""] <- "N"
-df$cs_vcrim [df$cs_vcrim == "1"] <- "Y"
-df$cs_bulge [df$cs_bulge == ""] <- "N"
-df$cs_bulge [df$cs_bulge == "1"] <- "Y"
-df$cs_other [df$cs_other == ""] <- "N"
-df$cs_other [df$cs_other == "1"] <- "Y"
-
-
-###########################################
-#     3) ANALYSIS
-############################################
-
-# logistic regression
-# to be done....
+# - logistic regression
+# - logistic regression with SDG (stochastic gradient descent) to deal with 7k regressors
 
 ##########################################
-#     4) PostProcessing
+#     3) PostProcessing
 ############################################
 
-# plots, tables, nice output etc
+# this part produces relevant tables, maps & graphs
 
+source("Graphs.R")
 
