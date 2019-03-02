2107fe1776daa3ff72a54e14a0414d8708985f52
Ling-Ki <34686016+Ling-Ki@users.noreply.github.com>
2019-03-02 19:13:19 +0100

    Update Graphs.R

--- Graphs.R
+++ Graphs.R
@@ -1,5 +1,8 @@
 setwd("D:/Ling Ki/Uni/Master/HU-MEMS/Sem 3/Statistidal Programming Language/Project/Racial Disparities")
 
+#*************************************
+#Preparation
+#*************************************
 
 #Install packages
 install.packages("arsenal")
@@ -9,7 +12,6 @@ install.packages("plyr")
 install.packages("tidyverse")
 install.packages("ggmap")
 install.packages("reshape")
-
 
 #Load packages
 library(arsenal)
@@ -23,83 +25,107 @@ library(tidyverse)
 library(ggmap)
 library(reshape)
 
+#*************************************
+#Data Processing
+#*************************************
 
-#Read in datasets
-  #Frisk reports
-ny13 <- read.csv("2013.csv", header=TRUE)
-ny14 <- read.csv("2014.csv", header=TRUE)
-ny15 <- read.csv("2015.csv", header=TRUE)
-ny16 <- read.csv("2016.csv", header=TRUE)
-  #Homicide reports
-all16 <- readLines("cy2016.csv")  #Read-in files as text
-skip16 <- all16[-c(1:4)] #Dropping the first 4 rows of table
-homi16 <- read.csv(textConnection(skip16), header=TRUE)
+#***********************
+#1. Read Datasets
+#***********************
 
-all17 <- readLines("cy2017.csv")  
-skip17 <- all17[-c(1:4)] 
-homi17 <- read.csv(textConnection(skip17), header=TRUE)
+#Frisk reports
+ny13 = read.csv("2013.csv", header=TRUE)
+ny14 = read.csv("2014.csv", header=TRUE)
+ny15 = read.csv("2015.csv", header=TRUE)
+ny16 = read.csv("2016.csv", header=TRUE)
 
+#Homicide reports
+all16  = readLines("cy2016.csv")  #Read in files as text
+skip16 = all16[-c(1:4)]           #Dropping the first 4 rows
+hmc16  = read.csv(textConnection(skip16), header=TRUE)
 
-#Create datasets
-  #Frisk reports
-info <- c("pct", "race", "crimsusp", "xcoord", "ycoord")  #Create list of info for data selection
-ny13 <- subset(ny13, select=info,  #Select only relevant columns
-               ny13$crimsusp %like% "CPW" | ny13$crimsusp %like%  "C.P.W" | ny13$crimsusp %like%  "WEAPON") #Select only CPW data
-ny14 <- subset(ny14, select=info,  
+all17  = readLines("cy2017.csv")  
+skip17 = all17[-c(1:4)] 
+hmc17  = read.csv(textConnection(skip17), header=TRUE)
+
+#***********************
+#2. Create Datasets
+#***********************
+
+#Frisk reports  (Check: Subset several data frames with one command)
+info = c("pct", "race", "crimsusp", "xcoord", "ycoord")                                                      #Create list of info for data selection
+ny13 = subset(ny13, select=info,                                                                             #Select only relevant columns
+               ny13$crimsusp %like% "CPW" | ny13$crimsusp %like%  "C.P.W" | ny13$crimsusp %like%  "WEAPON")  #Select only CPW data
+ny14 = subset(ny14, select=info,  
                ny14$crimsusp %like% "CPW" | ny14$crimsusp %like%  "C.P.W" | ny14$crimsusp %like%  "WEAPON")
-ny15 <- subset(ny15, select=info,  
+ny15 = subset(ny15, select=info,  
                ny15$crimsusp %like% "CPW" | ny15$crimsusp %like%  "C.P.W" | ny15$crimsusp %like%  "WEAPON")
-ny16 <- subset(ny16, select=info,  
+ny16 = subset(ny16, select=info,  
                ny16$crimsusp %like% "CPW" | ny16$crimsusp %like%  "C.P.W" | ny16$crimsusp %like%  "WEAPON")
-report <- rbind(ny13, ny14, ny15, ny16) #Create complete report
+report = rbind(ny13, ny14, ny15, ny16)                                                                       #Create complete report
 
-  #Homicide
-homi17 <- homi17[-(293:1917),-(26:99)]  #Remove empty columns and rows
-check_header <- cbind(colnames(homi16), colnames(homi17))  #View(check_header)  #Checked, OK
-check_type <- sapply(list(homi16,homi17), sapply, class)  #View(check_type)  #Checked, OK
-homicide <- rbind(homi16, homi17)  #Create complete dataset
+#Homicide
+hmc17        = hmc17[-(293:1917), -(26:99)]               #Remove empty columns and rows
+check_header = cbind(colnames(hmc16), colnames(hmc17))    #View(check_header)  #Checked, OK
+check_type   = sapply(list(hmc16,hmc17), sapply, class)   #View(check_type)  #Checked, OK
+hmc          = rbind(hmc16, hmc17)                        #Create complete dataset
 
+#*************************************
+#Overview
+#*************************************
 
-#Data overview
 str(report)
-unique(report$pct)  #Entries are normal
-unique(report$race) #12 levels of races. Need to combine / replace (?)
-any(is.na(report$xcoord)) #TRUE, there is NA
-any(is.na(report$ycoord)) #Also TRUE, there is NA
+unique(report$pct)        #Entries are normal
+unique(report$race)       #12 levels of races. Need to combine / replace (?)
+any(is.na(report$xcoord)) #TRUE
+any(is.na(report$ycoord)) #TRUE
 
+str(hmc)                  #Variable "PRECINCT" should be a string instead of integer
+hmc$PRECINCT = as.character(hmc$PRECINCT)
 
-str(homicide) #Variable "PRECINCT" should be a string instead of integer
-homicide$PRECINCT <- as.character(homicide$PRECINCT)
+#*************************************
+#Data Visualization
+#*************************************
 
+#***********************
+#1. CPW and Homicide
+#***********************
 
-#Plot Graphs - CPW and Homicide
-  #Preparation
-pp_cpw <- count(report, "pct")  #Count CPW by precinct
-names(pp_cpw)[2] <- "freq_cpw"
-pp_cpw$pct_cpw <- pp_cpw$freq_cpw / sum(pp_cpw$freq_cpw) * 100
+#Preparation
+pp_cpw           = count(report, "pct")                            #Count CPW by precinct
+names(pp_cpw)[2] = "freq_cpw"
+pp_cpw$pct_cpw   = pp_cpw$freq_cpw / sum(pp_cpw$freq_cpw) * 100
 
-pp_homicide <- count(homicide,"PRECINCT")  #Count homicide by precinct
-names(pp_homicide)[2] <- "freq_hmc"
-pp_homicide$pct_hmc <- pp_homicide$freq_hmc / sum(pp_homicide$freq_hmc) * 100
+pp_hmc           = count(hmc,"PRECINCT")                           #Count homicide by precinct
+names(pp_hmc)[2] = "freq_hmc"
+pp_hmc$pct_hmc   = pp_hmc$freq_hmc / sum(pp_hmc$freq_hmc) * 100
 
-joint <- merge.data.frame(pp_cpw, pp_homicide, by=intersect(names(pp_cpw), names(pp_homicide)),
-                        by.x = "pct", by.y = "PRECINCT", all.x = TRUE, sort = TRUE)
-joint[is.na(joint)]<-0  #Replace all NAs with 0
-str(joint)  #All normal
+joint            = merge.data.frame(pp_cpw, pp_hmc, by=intersect(names(pp_cpw), names(pp_hmc)), 
+                                       by.x="pct", by.y="PRECINCT", all.x=TRUE, sort=TRUE)
+joint[is.na(joint)] = 0                                            #Replace all NAs with 0
+str(joint)                                                         #All normal
   
-  #Plot grouped bar plot
-to_plot <- joint%>%
-  top_n(n=77,wt=pct_cpw)%>%
-  arrange(desc(pct_cpw))
+#Grouped Bar Plot
+data  = joint %>%
+          top_n(n=20,wt=pct_cpw) %>%
+          arrange(desc(pct_cpw))
+data  = rename(data, c("pct"="Precinct", "pct_cpw"="CPW", "pct_hmc"="Homicide"))
+data  = data[,-c(2, 4)]
 
-to_plot$freq_cpw <- NULL
-to_plot$freq_hmc <- NULL
+plot  = melt(data, id.vars='Precinct')
+plot  = rename(plot, c("value"="Percentage"))
 
-data <- melt(to_plot, id.vars='pct')
-
-ggplot(data, aes(x = pct, y = value, fill = variable)) + 
-  geom_bar(position = "dodge", stat = "identity") + theme(axis.text.x= element_text(angle=45, hjust=1)) + 
-  labs(title='Distribution of CPW versus Homicide')
+order = plot %>%
+          filter(variable=="Homicide") %>%
+          arrange(desc(Percentage)) %>%
+          .$Precinct %>% as.character
+  
+ggplot(plot, aes(x = Precinct, y = Percentage, fill = variable)) + 
+    geom_bar(position = "dodge", stat = "identity") + 
+    scale_x_discrete(limits=order) + 
+    xlab("Precinct") +
+    theme(axis.text.x= element_text(angle=45, hjust=1)) + 
+    labs(title='Distribution of CPW versus Homicide')
   
     #Individual plots
 ggplot(to_plot, aes(x=pct, y=pct_cpw))+
