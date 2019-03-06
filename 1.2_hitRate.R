1b180c35a6de31a894d678f3ab07f1d701f441ab
MHessenius <mhessenius@web.de>
2019-03-06 22:57:24 +0100

    hitRate2013

--- 1.2_hitRate.R
+++ 1.2_hitRate.R
@@ -49,5 +49,47 @@ for (yr in years){
   
 }
 
+### DIRTY SOLUTION FOR 2013 -> all excluded to save calculation time!
+
+# df_coord = df[c("year","long","lat","weaponfound")]
+# df2012 = df_coord[df_coord$year==2012,]
+# df2012_1 = df2012[1:10000,]
+# df2012_2 = df2012[10001:20000,]
+# df2012_3 = df2012[20001:30000,]
+# df2012_4 = df2012[30001:40000,]
+# df2012_5 = df2012[40001:50000,]
+# df2012_6 = df2012[50001:60000,]
+# df2012_7 = df2012[60001:nrow(df2012),]
+# 
+# # store all in a list
+# dflist = list(df2012_1,df2012_2,df2012_3,df2012_4,df2012_5,df2012_6,df2012_7)
+# 
+# # apply HitRateCalcution on every element of the list
+# output = lapply(dflist, function(x){
+#   df_yr_unweighted = geodist(df[df$year==2013,c("long","lat")], x[,c("long","lat")])/1000
+#   df_yr_unweighted = exp(-(df_yr_unweighted^2)/2)
+#   
+#   df_yr_wpfound = as.matrix(x$weaponfound)
+#   sums_weighted = df_yr_unweighted %*% df_yr_wpfound
+#   
+#   sums_unweighted = apply(df_yr_unweighted, 1, sum)
+#   
+#   return(sums_weighted/sums_unweighted)
+#   
+# })
+# 
+# # put list-elements together
+# test = as.data.frame(unlist(output[[1]]))
+# test$two = as.data.frame(unlist(output[[2]]))
+# test$three = as.data.frame(unlist(output[[3]]))
+# test$four = as.data.frame(unlist(output[[4]]))
+# test$five = as.data.frame(unlist(output[[5]]))
+# test$six = as.data.frame(unlist(output[[6]]))
+# test$seven = as.data.frame(unlist(output[[7]]))
+# test$hitRate2013 = apply(test,1,sum)
+# 
+# # add to dataset
+# df$hitRate[which(df$year==2013)] = test$hitRate2013
+
 # save Output
 saveRDS(df,file="df.rds")
