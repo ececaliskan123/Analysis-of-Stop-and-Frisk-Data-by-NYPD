6feb7c1c8bae48c7055345a670544ea6186c272a
Your Name <lingki.wong@gmail.com>
2019-03-09 23:46:04 +0100

    --

--- 3.2_FurtherAnalysis.R
+++ 3.2_FurtherAnalysis.R
@@ -1,1 +1,73 @@
+#*************************************
+#Preparation
+#*************************************
+
+#Install packages
+install.packages("dplyr")
+install.packages("foreign")
+install.packages("plyr")
+install.packages("ggmap")
+install.packages("geodist")
+install.packages("sf")
+
+#Load packages
+library(base)
+library(data.table)
+library(foreign)
+library(geodist)
+library(ggmap)
+library(ggplot2)
+library(sf)
+
+# Source codes
+source("1.0_FirstSteps.R", local = FALSE) 
+source("1.1_coordinates.R", local = FALSE)
+source("1.2_hitRate.R", local = FALSE)
+source("1.3_Cleaning.R", local = FALSE)
+
+#*************************************
+#Data Processing
+#*************************************
+
+#=============================
+#1. Read in Homicide Reports
+#=============================
+
+
+
+#=============================
+#2. Process Datasets
+#=============================
+
+
+
+#*************************************
+#Data Visualization
+#*************************************
+
+#=============================
+#1. Total Number of Stops over Time
+#=============================
+
+
+
+#=============================
+#2. Total Case Number of Weapon Found
+#=============================
+
+
+#=============================
+#3. Stops - Census Racial Compositions
+#=============================
+
+
+#=============================
+#4. 
+#=============================
+
+
+#=============================
+#5. 
+#=============================
+
 
