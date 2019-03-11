3a9b60e7ebbebec47f098013420e3e7a69b9768e
Ling-Ki <34686016+Ling-Ki@users.noreply.github.com>
2019-03-11 18:24:24 +0100

    Update LoadPackages.R

--- LoadPackages.R
+++ LoadPackages.R
@@ -8,6 +8,13 @@ if(!require("lubridate")) install.packages("lubridate"); library("lubridate")
 if(!require("anytime")) install.packages("anytime"); library("anytime")
 if(!require("glmnet")) install.packages("glmnet"); library("glmnet") 
 if(!require("glinternet")) install.packages("glinternet"); library("glinternet") 
-if(!require("dplyr")) install.packages("dplyr"); library("dplyr") 
 if(!require("ModelMetrics")) install.packages("ModelMetrics"); library("ModelMetrics") 
+if(!require("data.table")) install.packages("data.table"); library("data.table") 
+if(!require("foreign")) install.packages("foreign"); library("foreign") 
+if(!require("ggmap")) install.packages("ggmap"); library("ggmap")  # get map
+if(!require("tidyverse")) install.packages("tidyverse"); library("tidyverse") # call ggplot2, dplyr, etc.
+if(!require("reshape")) install.packages("reshape"); library("reshape") # to melt
+if(!require("devtools")) install.packages("devtools"); library("devtools") # source, install
+
+library(base)
 # to be continued
