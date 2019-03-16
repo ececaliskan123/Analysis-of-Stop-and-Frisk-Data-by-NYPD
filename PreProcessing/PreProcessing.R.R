83551b6a27d95916c41cb81f3ed81e48023292db
MHessenius <mhessenius@web.de>
2019-03-16 22:08:41 +0100

    adjust structure

--- PreProcessing/PreProcessing.R.R
+++ PreProcessing/PreProcessing.R.R
@@ -83,14 +83,87 @@ df[, c(weaponVars)] = NULL  #delete, since no use for weaponVars anymore
 df$height = df$ht_feet * 30.48 + df$ht_inch * 2.54
 df[, c("ht_feet", "ht_inch")] = NULL
 
-df$age = as.numeric(df$age)  # YOU CAN COPY THIS LINE INTO CLEANING!
+df$age = as.numeric(df$age)  # convert age to numeric
 
 # save row-name in a column as a unique identifier (easier to work with)
 if (!require("tibble")) install.packages("tibble")
-library("tibble")
 df = tibble::rownames_to_column(df)
 df$rowname = as.numeric(df$rowname)
 
+##### TRANSFORM COORDINATES ######
+
+# Define Spatial Data
+df = df[!is.na(df$xcoord) & !is.na(df$ycoord),]             # deletes NAs
+df = st_as_sf(df,coords=c("xcoord","ycoord"),crs=102718)   #define spatial data, "102718" is the code for the US state planning
+
+# store coordinates in long/lat format
+df      = st_transform(df, crs=4326) # switch to long/lat format
+df$long = st_coordinates(df)[,1] # add longitude (= x value)
+df$lat  = st_coordinates(df)[,2]  # add latitude (= y value)
+
+df      = st_set_geometry(df, NULL) # remove the spatial component
+
+#### CALCULATE LOCAL HIT RATE #######
+
+# load package to calculate (pairwise) geodesic distance
+if(!require("geodist")) install.packages("geodist"); library("geodist") # to calculate pairwise (pw) distances
+if(!require("pbapply")) install.packages("pbapply"); library("pbapply") # to track progress of lapply
+
+# define hitRate function
+
+hitRateCalculation = function(yr,blocksize){
+  
+  # create df(t) & df(t-1)
+  df_t  = df[df$year==yr,]
+  df_t1 = df[df$year==yr-1,]
+  
+  # split up df(t)
+  block   = blocksize
+  totRow  = nrow(df_t)
+  splits  = rep(1:ceiling(totRow/block),each=block)[1:totRow]
+  df_splitted = split(df_t,splits) #contains df_t splitted by block of rows
+  
+  
+  # use inside lapply:
+  df_t1_wpfound     = as.matrix(df_t1$weaponfound)  # weight the distances according to binary variable, i.e. creates the numerator
+  
+  # calculate the hit-Rate
+  
+  hitRate = pblapply(df_splitted, function(X){
+    gc() # garbage collection -> empty memory
+    step1 = geodist(X[,c("long","lat")], df_t1[,c("long","lat")])/1000
+    step1 = exp(-(step1^2)/2)
+    sums_unweighted = rowSums(step1)  #rowsum for block of rows
+    sums_weighted = step1 %*% df_t1_wpfound
+    hitRate = sums_weighted/sums_unweighted #for each chunk of rows
+  })
+  
+  return(unlist(hitRate))
+}
+
+# apply the hitrate function to all years
+years = unique(df$year)[unique(df$year)>min(unique(df$year))] # saves all years except the lowest (since this is only needed for the h(t-1) annotation)
+AllHRs = lapply(years, function(x){hitRateCalculation(x,500)}) # calculates hitRates for all yearas
+names(AllHRs) = years
+
+# AllHRs is a list with sub-lists -> unlist each sublist
+AllHRs = lapply(years, function(x){unlist(AllHRs[[paste(x)]])})
+names(AllHRs) = years
+
+# assign list elements to dataset
+
+df$hitRate = NA
+for(i in years){
+  test =as.data.frame(AllHRs[[paste(i)]])
+  df$hitRate[which(df$year==i)] = test[,1]
+}
+
+# delete the first year (since it was only necessary for hitRate calculation)
+
+firstYear = min(unique(df$year))
+df = df[df$year!=firstYear,]
 
 # SAVE FILE FOR FURTHER PROCESSING (cleaning etc.)
-saveRDS(df, file = "df.rds")
+saveRDS(df,file="df.rds")
+
+
