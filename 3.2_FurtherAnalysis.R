1cc816fff84d078c2fcb21c70ba6328a8b653c2b
Your Name <lingki.wong@gmail.com>
2019-03-16 01:50:34 +0100

    3.1 finalized

--- 3.2_FurtherAnalysis.R
+++ 3.2_FurtherAnalysis.R
@@ -77,12 +77,18 @@ source("2.3_RF.R", local = FALSE)
 #Model check
 #*************************************
 
+df2 = readRDS("3.0.rds")
+df2 = df2[ which(df2$year=='2015' | df2$year=='2016'),]
+
 rf = readRDS("", local = FALSE)
 lg = readRDS("", local = FALSE)
 
 # Include estimated hit rates from both models
-df2$rf.hit = rf$hitRate[match(df2$rowname, rf$rowname)]  # Paste hit rate from rf to df2 when "rowname" matches
+df2$rf.hit = rf$yhat.h2o[match(df2$rowname, rf$rowname)]  # Paste hit rate from rf to df2 when "rowname" matches
 df2$lg.hit = lg$hitRate[match(df2$rowname, lg$rowname)]
+df2$lg.hit = (df2$rf.hit + df2$hitRate) * 2 / 3
+
+df2        = subset(df2, rf.hit != "NA" & lg.hit != "NA")  # Remove entriew where rf.hit and lg.hit are not available
 
 # Plot Model hit rates against empirical hit rates
 
@@ -91,14 +97,46 @@ df2$lg.hit = lg$hitRate[match(df2$rowname, lg$rowname)]
 # Plot 3 Hit rate against (1) Age group, (2) Sex, (3) Race. Use facet to split
 
 # Define different age groups using ifelse statements
-df2$age.group = ifelse(df2$age.raw < 18, "Under 18",
+df2$age.group = ifelse(df2$age.raw < 18, "0 to 18",
                        ifelse((18 <= df2$age.raw & df2$age.raw <= 25), "18 to 25",
                               ifelse((26 <= df2$age.raw & df2$age.raw <= 32), "26 to 32",
                                      ifelse((33 <= df2$age.raw & df2$age.raw <= 40), "33 to 40",
-                                            "Over 40")
+                                            "40 or above")
                                      )
                               )
                        )
+#############################################################################
+age = df2 %>% dplyr::group_by(age.group) %>% 
+  dplyr::select(age.group, hitRate, rf.hit, lg.hit) %>%
+  summarise_all(funs(mean))  # Summarise all three hit rates per group_by
+age$type = rep("age", nrow(age))
+names(age)[1] = "class"
+sex = df2 %>% group_by(sex) %>% 
+  dplyr::select(sex, hitRate, rf.hit, lg.hit) %>%
+  summarise_all(funs(mean))
+sex$type = rep("sex", nrow(sex))
+race = df2 %>% group_by(race) %>% dplyr::select(race, hitRate, rf.hit, lg.hit) %>% summarise_all(funs(mean))
+race$type = rep("race", nrow(race))
 
-df2 %>% group_by(grp) %>% summarise_all(funs(mean)) # Summarise all three hit rates per group_by
+lst = list(age, sex, race)
+myReadTable<-function(x){
+  names(x)[1] <-c("class")
+  return(x)
+}
+
+for (i in seq_along(lst)){names(lst[[1]]) = c("class", "class", "class")}
+trial = lapply(lst,myReadTable)
+rbind(age, sex, race)
+################################################################################
+
+# Hit rate against (1) Age group
+age.plot = ggplot(age, aes(age.group)) + 
+  theme_bw() + 
+  coord_flip(ylim = c(0, 0.2)) +
+  geom_point(aes(y=hitRate, colour = "Empirical")) + 
+  geom_point(aes(y=rf.hit, colour = "RF")) + 
+  geom_point(aes(y=lg.hit, colour = "Logit")) + 
+  scale_color_discrete(name = " ") +
+  labs(x = "Hit Rate", y = "Age Group")
+ 
 
