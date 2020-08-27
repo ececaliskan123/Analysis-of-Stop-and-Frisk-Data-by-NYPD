2b50dbc5a4a0f8a079478b51ec43ca15d6738d77
Ece Caliskan <34170632+ececaliskan123@users.noreply.github.com>
2020-08-27 20:14:06 +0200

    Update 0.0_MainFile.R

--- 0.0_MainFile.R
+++ 0.0_MainFile.R
@@ -10,7 +10,7 @@ source("LoadPackages.R") # load all required packages
 #####################################
 #     1) Quantlet: PreProcessing
 #####################################
-# Author: Malte Hessenius
+
 
 # Get the Dataset & merging
 source("./PreProcessing/PreProcessing.R")  # -> output: df.rds    
@@ -18,7 +18,7 @@ source("./PreProcessing/PreProcessing.R")  # -> output: df.rds
 #####################################
 #     2) Quantlet: Cleaning
 #####################################
-# Author: Ece Caliskan
+
 source("./Cleaning/Cleaning.R")  # -> output: df.rds (now cleaned)
 
 
@@ -27,33 +27,32 @@ rm(list = ls()[! ls() %in% "df"]) #remove everything except df from the global e
 ########################################
 #     3) Quantlet: Exploratory
 ########################################
-# Author: LingKi Wong
+
 source("./Exploratory/Exploratory.R")
 
 
 ########################################
 #     4) Quantlet: Replications
 ########################################
-# Author: LingKi Wong
+
 source("./Replications/Replications.R")
 
 
 ########################################
 #     5) Quantlet: Logit
-########################################
-# Author: Ece Caliskan
+
 source("./Logit/Logit.R")
 
 ##########################################
 #     6) Quantlet: RandomForest
 ############################################
-# Author: Malte Hessenius
+
 source("./RandomForest/RandomForest.R")
 
 
 ##########################################
 #     7) Quantlet: ModelAssessment
 ############################################
-# Author: LingKi Wong
+
 source("./ModelAssessment/ModelAssessment.R")
 
